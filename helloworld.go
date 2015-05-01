1c559b8d21be422934ac625507d150bd254d2507
siyu <siyu@acfun.tv>
2015-05-01 14:10:56 +0900

    正常的demo,原因是main在执行结束后会强制关闭所有的routine

--- helloworld.go
+++ helloworld.go
@@ -9,14 +9,14 @@ func main() {
 	q := goruntines.Init(10)
 	go func(q *goruntines.Queue) {
 		for i := 0; i<20; i++ {
-			a := q.Get()
-			log.Println("get:",a,",index:",i)
+			q.Add(i)
+			q.Status()
+			log.Println("index:",i)
 		}
 	}(q)
 	for i := 0; i<20; i++ {
-		q.Add(i)
-		q.Status()
-		log.Println("index:",i)
+		a := q.Get()
+		log.Println("get:",a,",index:",i)
 	}
 
 	fmt.Println("main start")
